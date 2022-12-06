afbdc2197dd18caae55219ea90d3b55b8e9f617b
RennieNjuguna <rennynjuguna@gmail.com>
2022-12-06 21:30:27 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+function writeCards(names, gift = "suprise"){
+ let array = [];
+  for(let i = 0; i<names.length;i++){
+        array.push(`Thank you, ${names[i]}, for the wonderful ${gift} gift!`);    
+}
+return array; 
+}
+const countDown = (num) => {
+    while( num >= 0){
+        console.log(num);
+        num --;
+    }
+}
